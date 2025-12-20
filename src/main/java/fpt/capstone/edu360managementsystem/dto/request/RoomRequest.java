23cc2e6c29a209096cb521779ef87686189e7b5e
atuanhu <namtungfpt@gmail.com>
2025-12-20 16:29:08 +0700

    check validate of room

--- src/main/java/fpt/capstone/edu360managementsystem/dto/request/RoomRequest.java
+++ src/main/java/fpt/capstone/edu360managementsystem/dto/request/RoomRequest.java
@@ -1,23 +1,24 @@
 package fpt.capstone.edu360managementsystem.dto.request;
 
 import fpt.capstone.edu360managementsystem.enums.RoomStatus;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-
 @Data
 public class RoomRequest {
+
     @NotBlank(message = "Room name is required")
     private String name;
 
     @NotNull(message = "Capacity is required")
-    @Min(value = 1, message = "Capacity must be at least 1")
+    @Min(value = 1, message = "Sức chứa phải lớn hơn 0")
+    @Max(value = 60, message = "Sức chứa tối đa là 60 người")
     private Integer capacity;
 
     private RoomStatus status = RoomStatus.AVAILABLE;
-
 
     public String getName() {
         return name;
